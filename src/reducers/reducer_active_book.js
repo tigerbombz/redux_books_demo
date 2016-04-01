0f1c58c68973bd413147b4e7d6db85037d3fc035
tigerbombz <madd1778@yahoo.com>
2016-04-01 09:43:04 -0700

    trying to get click book function working

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,8 @@
+//State argument is not applicatin state, only the state this reducer is repsonsible for
+export default function(state = null, action) {
+  switch(action.type) {
+  case 'BOOK_SELECTED':
+    return action.payload;
+  }
+  return state;
+}
